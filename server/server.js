3852ca20d63491805c028b951b7d65b23f32d7f2
iwen-sue <grace2268@hotmail.com>
2024-04-03 16:35:23 -0700

    add imports

--- server/server.js
+++ server/server.js
@@ -1,4 +1,30 @@
 import express from 'express';
+import path from 'path';
+// Import necessary AWS SDK v3 packages
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, GetCommand, ScanCommand, DeleteCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+
+//Import aws cognito
+import { CognitoIdentityProviderClient, 
+    SignUpCommand, 
+    ConfirmSignUpCommand, 
+    InitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider';
+
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Initialize DynamoDB Client
+const ddbClient = new DynamoDBClient({ 
+    region: 'us-west-2',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }, });
+const docClient = DynamoDBDocumentClient.from(ddbClient); // Initialize DynamoDB Document Client
+
+//initialize cognito client
+const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-west-2' });
 
 const app = express();
 
