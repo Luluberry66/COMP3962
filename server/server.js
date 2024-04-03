36df1e32ee81c324fa507710be4994c7c040f10a
iwen-sue <grace2268@hotmail.com>
2024-04-03 16:29:59 -0700

    server setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from server!' });
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 
 const Home = () => {
+
+  useEffect(() => {
+    fetch("/api").then(res => res.json()).then(data => console.log(data))
+  }, [])
+
   return (
     <div>
       <Hero/>
