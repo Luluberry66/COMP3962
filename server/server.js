ecfc8586766264360a74cb40758ba4527349be11
iwen-sue <grace2268@hotmail.com>
2024-04-04 11:52:51 -0700

    listen to variables

--- server/server.js
+++ server/server.js
@@ -27,11 +27,12 @@ const docClient = DynamoDBDocumentClient.from(ddbClient); // Initialize DynamoDB
 const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-west-2' });
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
